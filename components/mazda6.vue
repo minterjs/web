7fc9c830f80a49553ceff5379fec99af00ecfafc
root <root@206011.simplecloud.ru>
2019-06-23 15:29:34 +0300

    modified

--- components/mazda6.vue
+++ components/mazda6.vue
@@ -1,13 +1,21 @@
 <template>
     <div class="mazda6-container">
         <h2>MAZDA6PLS</h2>
+        <p v-on:click="copy" class="how-to">Buy and delegate MAZDA6PLS or donate: Mx7926f1944b8faabb3b440b394543ae6f5b2f8f37</p>
         <div class="nupls"><img src="/my.gif"/><br/>ну pls</div>
     </div>
 </template>
 
 <script>
+    import copy from 'copy-to-clipboard';
     export default {
-        name: "mazda6"
+        name: "mazda6",
+        methods: {
+            copy() {
+                copy('Mx7926f1944b8faabb3b440b394543ae6f5b2f8f37');
+                alert('Address copied to clipboard!')
+            }
+        }
     }
 </script>
 
@@ -31,5 +39,12 @@
     h2 {
         font-size: 20vh;
         color: white;
+        text-shadow: 1px 1px 2px black
+    }
+
+    p.how-to {
+        cursor: pointer;
+        color: white;
+        text-shadow: 1px 1px 2px black;
     }
 </style>
